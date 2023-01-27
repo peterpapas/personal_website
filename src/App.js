94e8f6df881fddbd5d6f069dc70ca0f579036fd1
peterpapas1 <120545220+peterpapas1@users.noreply.github.com>
2023-01-27 17:23:33 +1100

    Added styles

--- src/App.js
+++ src/App.js
@@ -11,30 +11,21 @@ function App() {
   return (
     <div className="App">
       <div id="outer">
-        <div id="inner">
+        <div
+          id="inner"
+          className="bg-white rounded-xl bg-opacity-60 backdrop-filter backdrop-blur-lg p-12"
+        >
           <div id="bio">
             <h1 alt="Panos">Peter Papas</h1>
             <p className="text-sm">
-              Hey there! I'm Pano👋, a Junior Front-End Web developer based in
-              Melbourne and I'm excited to show you what I can do! I love
-              playing with web development and UX/UI design, and I always try to
-              bring my A-game to every project I work on. I've worked as a
-              Junior Front-End Web developer at &nbsp;
-              <a href="https://www.nintendo.com.au/">Nintendo</a>, where I was
-              responsible for maintaining and updating website content in
-              coordination with global stakeholders, and also involved in the
-              development and delivery of new features. Before that, I worked at
-              &nbsp;<a href="https://edcursion.com.au/">Edcursion</a> as a
-              Junior Fullstack developer where I contributed to the development
-              of a custom UI library and assisted with the creation of a backend
-              API. I'm currently pursuing my IT degree part-time and
-              fully-online at RMIT University to deepen my knowledge and
-              hands-on experience in the field. In my free time, I love
-              expanding my skill set by building personal projects and
-              experimenting with different languages. I'm always up for a
-              challenge and I'm excited to see what the future holds. I am a
-              playful, curious and willing learner, who is always eager to
-              explore new things and to take on new challenges.
+              Hey there! I'm Pano 👋, a Junior Front-End Web developer based in
+              Melbourne with experience at &nbsp;
+              <a href="https://www.nintendo.com.au/">Nintendo</a> and &nbsp;
+              <a href="https://edcursion.com.au/">Edcursion</a>. I enjoy web dev
+              and UX/UI design, currently pursuing an IT degree at RMIT
+              University. In my free time, I enjoy building personal projects
+              and expanding my skills. I'm always up for a challenge and eager
+              to explore new things.
             </p>
             <div id="links">
               <a href="https://github.com/peterpapas">
