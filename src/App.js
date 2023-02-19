7b68d46cc7e99077c5d686b1c0e5b1e52d59850e
peterpapas1 <30211228+peterpapas@users.noreply.github.com>
2023-02-19 13:04:06 +1100

    Animated the H1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { solid, brands } from "@fortawesome/fontawesome-svg-core/import.macro";
